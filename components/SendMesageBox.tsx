a664ad8d614bad810d19f7770254abd291365ff3
川村直也 <kawamuranaoya@kawamuranaoyanoMacBook-Pro.local>
2023-01-23 21:09:31 +0900

    メッセージの送信

--- components/SendMesageBox.tsx
+++ components/SendMesageBox.tsx
@@ -1,11 +1,31 @@
 import styles from "@/components/SendMessageBox.module.css";
+import { useMessages } from "@/hooks/useMessage";
+import React, { useState } from "react";
 
 const SendMessageBox = () => {
+  const [message, setMessage] = useState("");
+  const { postMessage } = useMessages();
+
+  const onClickSend = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const requestMessage = {
+      id: "exmaple",
+      body: message,
+      channelId: "1",
+      createdAt: new Date(),
+    }
+    postMessage(requestMessage);
+    setMessage("");
+  }
+  
   return (
     <div className={styles.body}>
       <div className={styles.header}></div>
-      <textarea className={styles["message-box"]}></textarea>
-      <button>Send</button>
+      <textarea 
+        className={styles["message-box"]}
+        value={message} 
+        onChange={(e) => setMessage(e.target.value)}
+      ></textarea>
+      <button onClick={onClickSend}>Send</button>
     </div>
   );
 };
